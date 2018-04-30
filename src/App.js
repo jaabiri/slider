d4dfc813332de157027780d2ca1cd22f0395fb65
itsTarik <tarik.lefi@gmail.com>
2018-04-30 14:06:30 +0100

    added responsivity control to the slider

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import Item from './Item';
-import Slider from './Slider'
+import Slider from './Slider';
 import data from './data';
 import './App.css';
 
 class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1 style={{ color: '#fff', textAlign: 'center' }}>this slider is reponsive</h1>
+				<div className="demo">
+					<Slider responsive>
+						{data.map(({ src, description }, index) => (
+							<Item src={src} description={description} key={index} />
+						))}
+					</Slider>
+				</div>
+				<div className="demo">
+					<Slider slidesToScroll={1} slidesVisible={2}>
+						{data.map(({ src, description }, index) => (
+							<Item src={src} description={description} key={index} />
+						))}
+					</Slider>
+				</div>
 
-  render() {
-    return (
-      <div>
-      <div className="demo">
-      <Slider>
-        {
-          data.map(({ src, description },index) => <Item src={src} description={description} key={index}/>)
-         }
-      </Slider>
-      </div>
-      <div className="demo">
-      <Slider  slidesToScroll= {1} slidesVisible= {2}>
-        {
-          data.map(({ src, description },index) => <Item src={src} description={description} key={index}/>)
-         }
-      </Slider>
-      </div>
-
-      <div className="demo">
-      <Slider  slidesToScroll= {3} slidesVisible= {3}>
-        {
-          data.map(({ src, description },index) => <Item src={src} description={description} key={index}/>)
-         }
-      </Slider>
-      </div>
-      </div>
-    );
-  }
+				<div className="demo">
+					<Slider slidesToScroll={3} slidesVisible={3}>
+						{data.map(({ src, description }, index) => (
+							<Item src={src} description={description} key={index} />
+						))}
+					</Slider>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
